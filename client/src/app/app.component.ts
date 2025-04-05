daeef769dbb369da7b2dfe1540c7892b26241075
Tommy <tommycrabtree@outlook.com>
2025-04-04 22:47:36 -0500

    End of Section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { environment } from '../environments/environment';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
