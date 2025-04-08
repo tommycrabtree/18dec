7e7f134cf673972131075d6807aaacd914f47f82
Tommy <tommycrabtree@outlook.com>
2025-04-07 23:52:07 -0500

    End of Section 7

---
+++ api/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+using api.Data;
+using api.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+public class BuggyController(DataContext context) : BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var thing = context.Users.Find(-1);
+
+        if (thing == null) return NotFound();
+
+        return thing;
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {
+        var thing = context.Users.Find(-1) ?? throw new Exception("A silly thing happened");
+
+        return thing;
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not the best request");
+    }
+
+}
